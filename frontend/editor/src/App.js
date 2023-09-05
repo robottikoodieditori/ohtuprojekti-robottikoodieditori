f780f7ff590b59022de1d172608eb8fd8018ed9f
HYTapio <tapio.uzal@helsinki.fi>
2023-09-05 13:13:19 +0300

    setup development environment

--- frontend/editor/src/App.js
+++ frontend/editor/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setdata] = useState({
+        name: "",
+        age: 0,
+        date: "",
+        programming: "",
+    });
+ 
+    useEffect(() => {
+        fetch("/data").then((res) =>
+            res.json().then((data) => {
+                setdata({
+                    name: data.Name,
+                    age: data.Age,
+                    date: data.Date,
+                    programming: data.programming,
+                });
+            })
+        );
+    }, []);
+ 
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>React and flask</h1>
+                <p>{data.name}</p>
+                <p>{data.age}</p>
+                <p>{data.date}</p>
+                <p>{data.programming}</p>
+ 
+            </header>
+        </div>
+    );
 }
-
+ 
 export default App;
