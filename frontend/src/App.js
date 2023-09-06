414bfb4f4ed33453d73908c993799452a2570b41
Samppa Salo <samppasalo16@gmail.com>
2023-09-06 16:59:53 +0300

    add broken syntax highlighting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,69 @@
 import React, { useState, useEffect } from "react";
 import comms from "./services/comms"
+import { createTheme } from '@uiw/codemirror-themes';
 import CodeMirror from "@uiw/react-codemirror";
+import {parser} from "./services/parser"
+import {LRLanguage} from "@codemirror/language"
+import { javascript } from '@codemirror/lang-javascript';
+import {styleTags, tags as t} from "@lezer/highlight"
 
+console.log(t)
+let parserWithMetadata = parser.configure({
+    props: [
+      styleTags({
+        Identifier: t.variableName,
+        Boolean: t.bool,
+        Number: t.number,
+        String: t.string,
+        Null: t.null,
+        Command: t.command,
+        LineComment: t.lineComment,
+        "( )": t.paren
+      })
+    ]
+  })
+
+
+const exampleLanguage = LRLanguage.define({
+    parser: parserWithMetadata,
+    languageData: {
+      commentTokens: {line: ";"}
+    }
+  })
+
+const myTheme = createTheme({
+    theme: 'light',
+    settings: {
+      background: '#ffffff',
+      foreground: '#75baff',
+      caret: '#5d00ff',
+      selection: '#036dd626',
+      selectionMatch: '#036dd626',
+      lineHighlight: '#8a91991a',
+      gutterBackground: '#fff',
+      gutterForeground: '#8a919966',
+    },
+    styles: [
+      { tag: t.comment, color: 'red' },
+      { tag: t.variableName, color: 'red' },
+      { tag: [t.string, t.special(t.brace)], color: 'red' },
+      { tag: t.number, color: 'green' },
+      { tag: t.bool, color: 'red' },
+      { tag: t.null, color: 'red' },
+      { tag: t.keyword, color: 'green' },
+      { tag: t.operator, color: 'red' },
+      { tag: t.className, color: 'red' },
+      { tag: t.definition(t.typeName), color: 'red' },
+      { tag: t.typeName, color: 'red' },
+      { tag: t.angleBracket, color: 'red' },
+      { tag: t.tagName, color: 'red' },
+      { tag: t.attributeName, color: 'red' },
+    ],
+  });
+
+
+const extensions = [exampleLanguage];
+  
 
  
 function App() {
@@ -29,10 +91,10 @@ function App() {
     const onChange = React.useCallback((value, viewUpdate) => {
         changeEditorValue(value)
       }, []);
+
  
     return (
         <div className="App">
-            {console.log(editorValue)}
             <header className="App-header">
                 <h1>React and flask</h1>
                 <p>{data.name}</p>
@@ -42,6 +104,8 @@ function App() {
  
             </header>
             <CodeMirror
+            theme={myTheme}
+            extensions={extensions}
             onChange={onChange}
             />
         </div>
