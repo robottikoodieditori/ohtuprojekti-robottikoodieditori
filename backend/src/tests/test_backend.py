ea91497b4ea8d75cc1b2f905319234f1ac512a2b
Iida Björn <iida.w.bjorn@gmail.com>
2023-09-07 13:23:39 +0300

    import changes

--- backend/src/tests/test_backend.py
+++ backend/src/tests/test_backend.py
@@ -1,8 +1,9 @@
 from test_backend import __version__
-from tests.conftest import client
+
 
 def test_version():
     assert __version__ == '0.1.0'
+
 
 def test_simple_request(client):
     response = client.get("/data")
