a0d97f50922b1166b0ccb525d9b529f6c22b4dee
Meeri <meeri.simpanen@helsinki.fi>
2023-09-12 09:25:58 +0300

    cleaning

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -3,7 +3,7 @@ import '../index.css'
 const Navbar = () => {
     return (
         <div className={'navbar'}>
-            <p>Onpi juupi</p>
+            <p>Koodieditori</p>
         </div>
     );
 }
