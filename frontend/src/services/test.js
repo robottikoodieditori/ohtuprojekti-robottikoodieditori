ec0bd9f0751a5f5cebf62381314b37695e3e1541
mcdongo <81057645+mcdongo@users.noreply.github.com>
2023-09-13 14:48:20 +0300

    Delete frontend/src/services/test.js

--- frontend/src/services/test.js
+++
@@ -1,8 +0,0 @@
-const { parser } = require("./parser")
-
-const input = "string(1)"
-const parsed = parser.parse(input)
-
-parsed.iter().forEach(({ type, from, to }) => {
-    console.log(`Token: ${type.name} (${input.slice(from, to)})`)
-})
