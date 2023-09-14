9e25c23d89de839ada890ce5e1869d23ec586594
Leevi Kokkinen <leevi.kokkinen@gmail.com>
2023-09-14 09:42:54 +0300

    Add hover tooltips

--- frontend/src/utils/cmConfig.js
+++ frontend/src/utils/cmConfig.js
@@ -1,18 +1,10 @@
-import { createTheme } from '@uiw/codemirror-themes';
 import {parser} from "../services/parser"
 import {LRLanguage} from "@codemirror/language"
 import {styleTags, tags as t} from "@lezer/highlight"
 
-let parserWithMetadata = parser.configure({
-    props: [
-        styleTags({
-            Keyword: t.keyword,
-        })
-    ]
-});
 
 const Logo = LRLanguage.define({
-    parser: parserWithMetadata,
+    parser: parser,
     languageData: {
         commentTokens: {line: ";"}
     }
