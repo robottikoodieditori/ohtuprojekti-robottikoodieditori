952857d28a4df50caff99f168f99938fe8884106
Iida Björn <iida.w.bjorn@gmail.com>
2023-09-15 17:47:05 +0300

    changed .type command

--- frontend/cypress/e2e/editor_app.cy.js
+++ frontend/cypress/e2e/editor_app.cy.js
@@ -11,28 +11,28 @@ describe('Editor ', function() {
 
   it('syntax is highlighted', function() {
     cy.visit('http://localhost:3000')
-    cy.get("#editor").type("{ctrl}a").type("eteen hellou")
+    cy.get("#editor").type("{selectall}{backspace}").type("eteen hellou")
     cy.contains("eteen").should("have.css", "color", "rgb(255, 0, 0)")
     cy.contains("hellou").should("have.css", "color", "rgb(0, 0, 255)")
   })
 
   it('syntax is highlighted reverse', function() {
     cy.visit('http://localhost:3000')
-    cy.get("#editor").type("{ctrl}a").type("hellou eteen")
+    cy.get("#editor").type("{selectall}{backspace}").type("hellou eteen")
     cy.contains("eteen").should("have.css", "color", "rgb(255, 0, 0)")
     cy.contains("hellou").should("have.css", "color", "rgb(0, 0, 255)")
   })
 
   it('hovering reveals documentation', function() {
     cy.visit('http://localhost:3000')
-    cy.get("#editor").type("{ctrl}a").type("eteen")
+    cy.get("#editor").type("{selectall}{backspace}").type("eteen")
     cy.contains("eteen").click()
     cy.contains("lauseke et lauseke")
   })
 
   it('hovering on non keyword does not reveal documentation', function() {
     cy.visit('http://localhost:3000')
-    cy.get("#editor").type("{ctrl}a").type("hellou")
+    cy.get("#editor").type("{selectall}{backspace}").type("hellou")
     cy.contains("hellou").click()
     cy.contains("lauseke et lauseke").should('not.exist')
   })
