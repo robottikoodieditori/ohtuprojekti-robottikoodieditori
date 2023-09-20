72e1611bac0d8b2aca04db08451602d734bee5a4
Jussi Rantala <jussi.v.rantala@helsinki.fi>
2023-09-20 14:56:49 +0300

    Refactored sidebar, added hover effect to clickable words

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -1,24 +1,12 @@
 import '../index.css';
-import { useState, useEffect } from 'react';
-import docs from "../services/tooltips.json"
-import keywords from "../utils/keywords_finnish.txt"
-import ReactMarkdown from "react-markdown";
+import { useState } from 'react';
+import CommandList from './commandList';
+import Searchbar from './searchbar';
+import OneCommand from './oneCommand';
 
 const Sidebar = () => {
-    const [commands, setCommands] = useState([]);
     const [selectedCommand, setSelectedCommand] = useState(null);
     const [searchTerm, setSearchTerm] = useState('');
-    const [suggestions, setSuggestions] = useState([]);
-
-    useEffect(() => {
-        fetch(keywords)
-            .then((response) => response.text())
-            .then((text) => {
-                const commandsArray = text.split(',').filter(Boolean);
-                setCommands(commandsArray);            
-            })
-            .catch((error) => console.error('Error reading commands:', error));
-    }, []);
 
     const handleCommandClick = (command) => {
         setSelectedCommand(command);
@@ -27,58 +15,17 @@ const Sidebar = () => {
     const handleSearchChange = (event) => {
         const term = event.target.value;
         setSearchTerm(term);
-
-        if (term.trim() === '') {
-            setSuggestions([]);
-            return;
-        }
-
-        const matchingCommands = commands.filter(command => command.includes(term));
-        setSuggestions(matchingCommands);
-    };
-
-    const handleSuggestionClick = (suggestion) => {
-        setSelectedCommand(suggestion);
-        setSearchTerm('');
-        setSuggestions([]);
     };
 
     return (
         <div className="sidebar" id='sidebar'>
             <div className='content'>
                 {selectedCommand ? (
-                    <div>
-                        <button className='buttonsidebar' onClick={() => setSelectedCommand(null)}>Takaisin</button>
-                        <h2>{selectedCommand}</h2>
-                        <ReactMarkdown>{docs[selectedCommand]}</ReactMarkdown>
-                    </div>
+                    <OneCommand selectedCommand={selectedCommand} setSelectedCommand={setSelectedCommand} />
                 ) : (
                     <div>
-                        <div className="search-bar" id='searchbar'>
-                            <input
-                                type="text"
-                                placeholder="Etsi käskyä"
-                                value={searchTerm}
-                                onChange={handleSearchChange}
-                            />
-                        </div>
-                        <h2>Käskyt</h2>
-                        <ul>
-                            {searchTerm === '' ? (
-                                commands.map((command) => (
-                                    <li key={command}>
-                                        <button className="buttonsidebar" onClick={() => handleCommandClick(command)}>{command}</button>
-                                    </li>
-                                ))
-                            ) : (
-                                suggestions.map((suggestion) => (
-                                    <li key={suggestion}>
-                                        <button className="buttonsidebar" onClick={() => handleSuggestionClick(suggestion)}>{suggestion}</button>
-                                    </li>
-                                ))   
-
-                            )}
-                        </ul>
+                        <Searchbar searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
+                        <CommandList searchTerm={searchTerm} handleCommandClick={handleCommandClick} />
                     </div>
                 )}
             </div>
@@ -88,4 +35,3 @@ const Sidebar = () => {
 
 export default Sidebar;
 
-  
