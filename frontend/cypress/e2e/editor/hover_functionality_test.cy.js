756985d8084ff77937310604a102456d0e772cee
HYTapio <tapio.uzal@helsinki.fi>
2023-09-20 10:01:33 +0300

    changed test structure

--- frontend/cypress/e2e/editor/hover_functionality_test.cy.js
+++ frontend/cypress/e2e/editor/hover_functionality_test.cy.js
@@ -11,19 +11,14 @@ describe('Hover Functionality', function() {
     });
 
     it('does not show documentation for non-command words', function() {
+
         const commands = ['tulosta', 'eteen', 'taakse', 'vasemmalle', 'oikealle', 'jos', 'riippuen', 'olkoon', 'miten', 'anna', 'toista', 'luvuille'];
 
         cy.get("#editor").type('{selectall}').type('nonCommandWord');
         cy.contains('nonCommandWord').click();
         cy.wait(100);
         commands.forEach(command => {
-            cy.get('body').should('not.contain', command);
+            cy.get('#editor').should('not.contain', command);
         });
-
-        // Verifying, hovering over a command should show documentation
-        cy.get("#editor").type('{selectall}').type(commands[0]); // 'tulosta' for example
-        cy.contains(commands[0]).click();
-        cy.wait(100);
-        cy.contains(commands[0]);
     });
 });
