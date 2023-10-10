7e7bef552d87f192f93e67ec7ac21767328e848f
Leevi Kokkinen <leevi.kokkinen@gmail.com>
2023-10-10 10:16:14 +0300

    Display error messages from mockcompiler

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,16 +1,11 @@
 # pylint: skip-file
 
 # Import flask and datetime module for showing date and time
-import datetime
 import json
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 from random import randint
 from mockcompiler import MockCompiler
 
-
-from uselogomotion import main as uselogomotion
-
-x = datetime.datetime.now().strftime("%H:%M:%S")
 
 # Initializing flask app
 app = Flask(__name__, static_folder='../build/static', template_folder='../build')
@@ -22,7 +17,7 @@ def get_time():
 
     # Returning an api for showing in  reactjs
     return {
-        "Date": x,
+        "Date": ""
     }
 
 
@@ -31,11 +26,9 @@ def send_to_compiler():
     data = request.data 
     data = data.decode('UTF-8').replace("'", '"')
     data = json.loads(data)
-    # print(data['data'])
-    # print(f"{data['data']}, | {str(data['data'])}")
     errors = MockCompiler.compile(data['data'], 'eetvartti/ompi')
     print(errors)
-    return data
+    return errors
 
 @app.route('/')
 def index():
