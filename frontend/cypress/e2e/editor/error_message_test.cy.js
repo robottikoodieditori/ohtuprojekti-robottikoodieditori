a8c399acf8ac259e5019c0e10296d15d15a23ee0
Iida Björn <iida.w.bjorn@gmail.com>
2023-10-12 13:33:47 +0300

    test for message box

---
+++ frontend/cypress/e2e/editor/error_message_test.cy.js
@@ -0,0 +1,18 @@
+describe('Error Message Functionality', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000');
+        cy.get('#popup').contains('Kirjaudu').click(500);
+    });
+
+    it('should display error messages appropriately', () => {
+        // Scenario: Validate Error Message in Finnish
+        cy.get('[data-testid="toggleLanguageButton"]').invoke('text').then((text) => {
+            if(text.includes('Switch to English')) {
+                // If English, send a message and check response text
+                cy.get('#editor').type('{selectall}').type('Tämä viesti lähetetään.');
+                cy.get('#SENDBUTTON').click().wait(100);
+                cy.get('#sResponse').should('contain', 'Palvelin vastasi:');
+            }
+        });
+    });
+});
