3aa5c2aa37a7d7e037dfa053c83e867efe742e58
Joonas Maisala <joonasmaisala@Joonas-MacBook-Air.local>
2023-10-12 13:00:34 +0300

    test for error message box translation

---
+++ frontend/cypress/e2e/editor/error_message_translation_test.cy.js
@@ -0,0 +1,28 @@
+describe('Error Message Translation Functionality', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000');
+        cy.get('#popup').contains('Kirjaudu').click(500);
+    });
+
+    it('should display error messages in English and Finnish appropriately', () => {
+        // Scenario: Validate Error Message in English
+        // Ensure English is set by checking the toggle button text
+        cy.get('[data-testid="toggleLanguageButton"]').invoke('text').then((text) => {
+            if(text.includes('Vaihda suomeksi')) {
+                // If English, send a message and check response text
+                cy.get('#editor').type('{selectall}').type('This message will be sent.');
+                cy.get('#SENDBUTTON').click().wait(100);
+                cy.get('#sResponse').should('contain', 'Palvelin vastasi:');
+            }
+        });
+
+        // Scenario: Validate Error Message in Finnish
+        // Switch language
+        cy.get('[data-testid="toggleLanguageButton"]').click();
+
+        // Check the message in Finnish
+        cy.get('#editor').type('{selectall}').type('Tämä viesti lähetetään.');
+        cy.get('#SENDBUTTON').click().wait(100);
+        cy.get('#sResponse').should('contain', 'Server responded:');
+    });
+});
