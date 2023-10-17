b456b4df5f762150e6d76c836d6ac05a00e9e08b
Jussi Rantala <jussi.v.rantala@helsinki.fi>
2023-10-17 13:10:11 +0300

    mock compiler saves the code to file, refactored mock error detection

--- backend/src/mockcompiler.py
+++ backend/src/mockcompiler.py
@@ -12,7 +12,7 @@ class MockCompiler:
     def compile(code: str, output_file: str):
         errors = []
         # pylint: disable=anomalous-backslash-in-string
-        wordlist = re.split('\s|\n', code)
+        wordlist = re.split("\s|\n", code)
         wordlist = list(filter(lambda x: x != "", wordlist))
         print(wordlist)
         # pylint: disable=consider-using-enumerate
@@ -21,13 +21,18 @@ class MockCompiler:
                 summa = 0
                 for j in range(i):
                     summa += len(wordlist[j])
-                errors.append({"message": "errorroror", "start": summa,
-                               "end": summa + len(wordlist[i])})
+                errors.append(
+                    {
+                        "message": "errorroror",
+                        "start": summa,
+                        "end": summa + len(wordlist[i]),
+                    }
+                )
 
-        if session.get("username", 0)=="":
-            name="Tuntematon"
+        if session.get("username", 0) == "":
+            name = "Tuntematon"
         else:
-            name=session.get("username", 0)
+            name = session.get("username", 0)
 
         path = os.getcwd()
         path = os.path.join(path, "javafiles", name)
@@ -42,29 +47,41 @@ class MockCompiler:
         return errors
 
     @staticmethod
-    def compile2(code: str, output_file: str):
+    def create_error_list(lines_of_logo):
         errors = []
-        wordlist = code.split('\n')
-        # pylint: disable=too-many-nested-blocks
-        for index, value in enumerate(wordlist):
-            error_start = None
+        pattern = r"(?:\s|^)([^\s]*[A-Z][^\s]*)(?=\s|$)"
 
-            for m_i, m_v in enumerate(value):
-                if not error_start:
-                    if m_v == m_v.upper() and m_v != ' ':
-                        error_start = m_i+1
-                else:
-                    if m_v == ' ' or m_i == len(value) - 1:
-                        errors.append({"line":index+1, "message":"errorror",
-                                       "start":error_start, "end": m_i})
-                        error_start = None
+        for line_number, line in enumerate(lines_of_logo):
+            matches = re.finditer(pattern, line)
 
-            if error_start:
-                # pylint: disable=undefined-loop-variable
-                errors.append({"line":index+1, "message":"errorror",
-                               "start":error_start, "end":m_i})
-                error_start = None
-            print(output_file)
+            for match in matches:
+                errors.append(
+                    {
+                        "line": line_number + 1,
+                        "message": "errorror",
+                        "start": match.start(1) + 1,
+                        "end": match.end(1),
+                    }
+                )
+
+        return errors
+
+    @staticmethod
+    def compile2(code: str, output_file: str):
+        errors = MockCompiler.create_error_list(code.split("\n"))
+
+        username = session.get("username", 0) or "Tuntematon"
+
+        path = os.getcwd()
+        path = os.path.join(path, "javafiles", username)
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        path = os.path.join(path, output_file)
+
+        with open(path, "w") as f:
+            f.write(code)
 
         print(errors)
 
--- backend/src/server.py
+++ backend/src/server.py
@@ -8,41 +8,37 @@ from users import User
 
 
 # Initializing flask app
-app = Flask(__name__, static_folder='../build/static',
-            template_folder='../build')
+app = Flask(__name__, static_folder="../build/static", template_folder="../build")
 app.secret_key = "123"
 
 # Route for seeing a data
 
 
-@app.route('/data')
+@app.route("/data")
 def get_time():
-
     # Returning an api for showing in  reactjs
-    return {
-        "Date": ""
-    }
+    return {"Date": ""}
 
 
-@app.route('/send/compiler', methods=['POST'])
+@app.route("/send/compiler", methods=["POST"])
 def send_to_compiler():
     data = request.data
-    data = data.decode('UTF-8').replace("'", '"')
+    data = data.decode("UTF-8").replace("'", '"')
     data = json.loads(data)
-    errors = MockCompiler.compile2(data['data'], 'Koodi')
-    #print(errors)
+    errors = MockCompiler.compile2(data["data"], "Koodi")
+    # print(errors)
     return errors
 
 
-@app.route('/send/name', methods=['POST'])
+@app.route("/send/name", methods=["POST"])
 def send_name():
     data = request.data
     data = data.decode()
     data = json.loads(data)
-    User(data['name'])
+    User(data["name"])
     return User.get_user()
 
 
 # Running app
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
