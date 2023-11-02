87be1206eceb08e16fa726c4cde2833983b9be4b
Samppa Salo <samppasalo16@gmail.com>
2023-11-02 13:10:02 +0200

    modify tests

--- backend/src/tests/user_test.py
+++ backend/src/tests/user_test.py
@@ -1,11 +1,30 @@
 import unittest
 from db import DB
 import user_service
+import sqlite3
+import os
 
 class TestUser(unittest.TestCase):
     def setUp(self):
+        try:
+            os.remove("test_db.db")
+        except:
+            pass
         self.db = DB("test_db.db")
-        self.db.delete_from_db()
+        con = sqlite3.connect("test_db.db")
+        cur = con.cursor()
+        cur.execute('''CREATE TABLE users (
+            id INTEGER PRIMARY KEY,
+            name TEXT UNIQUE,
+            password TEXT
+        )''')
+        cur.execute('''CREATE TABLE logofiles (
+            id INTEGER PRIMARY KEY,
+            content TEXT,
+            user_id INTEGER REFERENCES users
+        )''')
+        
+        con.commit()
         
 
     def test_register(self):
