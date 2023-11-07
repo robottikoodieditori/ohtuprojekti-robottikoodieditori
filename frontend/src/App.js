0a6d402221ac3c7a3acbd54f479d69c91bbe13e4
Joonas Maisala <joonasmaisala@Joonas-MacBook-Air.local>
2023-11-07 09:36:07 +0200

    Admin view layout 2.1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Sidebar from "./components/sidebar";
 import Navbar from "./components/navbar";  
 import { LanguageProvider } from './contexts/languagecontext';
 import Tokenpopup from "./components/popup";
-import AdminView from "./components/adminView"; // Import AdminView
+import AdminView from "./components/adminView"; 
 import './css/footer.css'
 
 function App() {
