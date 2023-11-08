0d5c7c559d98b4d690bdac4c5a279a2fe84ed619
HYTapio <tapio.uzal@helsinki.fi>
2023-11-08 12:37:38 +0200

    Fixed sql multiple inserts

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, send_from_directory, jsonify
 from os import getenv
+from flask import Flask, request, send_from_directory, jsonify
 from mockcompiler import MockCompiler
 from user_service import UserService
 from file_service import FileService
@@ -34,25 +34,6 @@ def send_to_compiler():
     return jsonify(errors)
 
 
-@app.route("/send/name", methods=["POST"])
-def send_name():
-    content = request.json
-    print(data)
-    return jsonify({
-        'status': 'OK',
-        'name': content['name']
-    })
-
-
-
-@app.route("/send/getfile", methods=["POST"])
-def send_content():
-    data = request.json
-    return jsonify({
-        'status': 'OK',
-        'content': data['filename']
-        })
-
 @app.route("/login", methods=["POST"])
 def login():
     content = request.json
@@ -62,39 +43,42 @@ def login():
             "username": content["username"],
             "token": token
         })
-    else:
-        result = user_service.register(content["username"], content["password"])
-        if result:
-            token = user_service.login(content["username"], content["password"])
-            if token:
-                return jsonify({
-                    "username": content["username"],
-                    "token": token
-                })
-            else:
-                return "Invalid Credentials", 400
 
-        else:
-            return "Username already taken", 400
+    result = user_service.register(content["username"], content["password"])
+    if result:
+        token = user_service.login(content["username"], content["password"])
+        if token:
+            return jsonify({
+                "username": content["username"],
+                "token": token
+            })
+
+        return "Invalid Credentials", 400
+
+
+    return "Username already taken", 400
 
 
 @app.route("/user/files", methods=["POST"])
 def get_user_files():
     content = request.json
-    files = user_service.get_user_files(content['username'], content['password'])
-    print(files)
-    return jsonify(files)
+    user_id = user_service.verify_token(content['token'])
+    if user_id:
+        result = file_service.get_user_files(user_id)
+        return jsonify(result)
+    return "Invalid Credentials", 400
+
 
 
 @app.route("/user/save", methods=["POST"])
 def save_file():
     content = request.json
-    id = user_service.verify_token(content['token'])
-    if id:
+    user_id = user_service.verify_token(content['token'])
+    if user_id:
         result = file_service.save_file(
-            content['filename'], content['textContent'], id)
-
-    return jsonify({result: result})
+            content['filename'], content['textContent'], user_id)
+        return jsonify({result: result})
+    return "Invalid Credentials", 400
 
 
 # Running app
