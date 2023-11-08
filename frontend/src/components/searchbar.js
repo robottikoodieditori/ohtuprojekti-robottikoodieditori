53f79a376e6232754fbe8ab171340bce4754578a
Meeri <meeri.simpanen@helsinki.fi>
2023-11-08 10:14:47 +0200

    accessibility labels
    Co-authored-by: iidaw <iida.w.bjorn@gmail.com>

--- frontend/src/components/searchbar.js
+++ frontend/src/components/searchbar.js
@@ -1,17 +1,20 @@
 import "../css/index.css";
 
 const Searchbar = ({ searchTerm, handleSearchChange, translations }) => {
-    console.log("Translations in Searchbar:", translations);
     return (
-        <div className="search-bar" id='searchbar'>
-            <input
-                type="text"
-                placeholder={translations.searchPlaceholder}
-                value={searchTerm}
-                onChange={handleSearchChange}
-            />
-        </div>
-    )
-}
-
-export default Searchbar;
+      <div className="search-bar" id='searchbar'>
+        <label htmlFor="searchInput" style={{ display: 'none' }}>
+          {translations.searchPlaceholder}
+        </label>
+        <input
+          type="text"
+          id="searchInput"
+          placeholder={translations.searchPlaceholder}
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+      </div>
+    );
+  }
+  
+  export default Searchbar;
