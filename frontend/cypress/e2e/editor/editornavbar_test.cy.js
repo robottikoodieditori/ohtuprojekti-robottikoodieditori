3906bebc81c0126bb2661463026520f5c1e77d25
Joonas Maisala <joonasmaisala@Joonas-MacBook-Air.local>
2023-11-14 15:25:37 +0200

    editornavbar_test.cy.js test fix

--- frontend/cypress/e2e/editor/editornavbar_test.cy.js
+++ frontend/cypress/e2e/editor/editornavbar_test.cy.js
@@ -2,28 +2,28 @@ describe('Login functionality', function() {
     beforeEach(function() {
         cy.visit('http://localhost:3000');
         cy.get('#registration-name-input').type('Jaakko')
-        cy.get('#popup').contains('Kirjaudu').click(500)
+        cy.get('#popup').contains('Kirjaudu').click(50)
     });
 
     it('Attempting to make a new file', function() {
         cy.get('#editor').type('oikealle 20')
-        cy.get('#editornavbar').contains('Uusi Tiedosto').click(500)
+        cy.get('#editornavbar').contains('Uusi Tiedosto').click(50)
         cy.get("#editor").contains('Logo...')
     })
 
     it('Attempting to save a new file', function() {
         cy.get('#editor').type('eteen 10')
-        cy.get('#editornavbar').contains('Tallenna').click(500)
+        cy.get('#editornavbar').contains('Tallenna').click(50)
         cy.get('#content-saveNew').contains('Anna uusi tiedostonimi')
         cy.get('#newFileNameInput').type('testi', {force:true})
-        cy.get('#content-saveNew').contains('Tallenna nimellä').click(500)
+        cy.get('#content-saveNew').contains('Tallenna nimellä').click(50)
         cy.get("#editor").should('contain', 'eteen 10')
     })
 
     it('Attempting to open a file', function() {
-        cy.get('#editornavbar').contains('Avaa Tiedosto').click(500)
+        cy.get('#editornavbar').contains('Avaa Tiedosto').click(50)
         cy.get("#overlay").contains('Valitse tiedosto')
-        cy.get("#overlay").contains('testi').click(500)
+        cy.get("#overlay").contains('testi').click(50)
         cy.get("#editor").should('contain', 'eteen 10')
     })
 })
