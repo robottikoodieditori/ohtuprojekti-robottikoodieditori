950abdd04e0654d33c123f6c866c71d0184f53a4
Meeri <meeri.simpanen@helsinki.fi>
2023-11-23 13:07:42 +0200

    something we forgot before
    Co-authored-by: iidaw <iida.w.bjorn@gmail.com>

--- frontend/cypress/e2e/editor/admin_functionality_test.cy.js
+++ frontend/cypress/e2e/editor/admin_functionality_test.cy.js
@@ -26,7 +26,7 @@ describe('Admin functionality', function() {
         cy.get('.user-info').should('be.visible')
         cy.get('.user-info p:contains("Käyttäjä:")').should('contain', 'Alice')
         cy.get('.user-info p:contains("Salasana:")').should('contain', 'password123')
-        cy.get('.delete-user-button').should('be.visible')
+        cy.get('.change-password-button').should('be.visible')
     })
 
     it('Attempting to show student files (ENGLISH)', function() {
