76f68887353e980d421c8e7b965562121ff5534f
Leevi Kokkinen <leevi.kokkinen@gmail.com>
2023-11-28 12:26:49 +0200

    Refactor editornavbar.js into several components

--- frontend/src/components/editorNavbar.js
+++ frontend/src/components/editorNavbar.js
@@ -5,7 +5,8 @@ import { handleFile } from '../reducers/commsReducer';
 import { setFileName, setContent, setFileId, resetFile } from "../reducers/editorReducer";
 import commService from "../services/comms";
 import '../css/editornavbar.css';
-import Popup from 'reactjs-popup';
+import FileSelectionScreen from "./editorNavbarFileSelectionScreen";
+import NewFileScreen from "./editorNavbarNewFileScreen";
 
 
 const EditorNavbar = () => {
@@ -91,99 +92,30 @@ const EditorNavbar = () => {
         }
     }
 
-    const NewFileScreen = () => {
-        return (
-            <div className="overlay" id="overlay" >
-                <Popup
-                    open= {isNewFileOpen}
-                    closeOnDocumentClick={false}
-                    overlayStyle={{ background: 'rgba(0,0,0,0.8'}}
-                >
-                    <div className='content-saveNew' id="content-saveNew" role = "dialog" aria-label="new file window">
-                        <div className="content-saveNew-header ">
-                            <h2 tabIndex="0">{translations?.editorNavbar.filenamePlaceholder}</h2>
-                            <div className='saveNew-header'>
-                                <button className="close-button-saveNew" onClick={() => setisNewFileOpen(false)}>X</button>
-                            </div>
-                        </div>
-                        <form onSubmit={handleSaveNew}>
-                            <label>                    
-                                <input
-                                    type="text"
-                                    placeholder={
-                                        translations?.editorNavbar.filenamePlaceholder
-                                    }
-                                    id='newFileNameInput'
-                                />
-                            </label>
-                            <div className="content-saveNew-submit-button">
-                                <button type='submit'>
-                                    {translations?.editorNavbar.saveWithName}
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </Popup>
-            </div>
-        )
-    }
-
-    const FileSelectionScreen = () => {
-        return (
-            <div className="overlay" id="overlay">
-                <Popup
-                    open= {isFileSelectOpen}
-                    closeOnDocumentClick={false}
-                    overlayStyle={{ background: 'rgba(0,0,0,0.8'}}
-                >
-                    <div className='content-file-select' id="content-file-select" role = "dialog" aria-label="choose file window">
-                        <div className="content-file-select-header">
-                            <h2 tabIndex="0">{translations?.editorNavbar.chooseFile}</h2>
-                            <button className='close-button-file-select' onClick={() => setisFileSelectOpen(false)}>X</button>
-                        </div>
-                        { fileList && (
-                            <div>
-                                <table>
-                                    <thead>
-                                        <tr>
-                                            <th></th>
-                                            <th className="center-th">{translations?.editorNavbar.fileName}</th>
-                                            <th className="center-th">{translations?.editorNavbar.createdAt}</th>
-                                            <th className="center-th">{translations?.editorNavbar.lastEdited}</th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {fileList.map(file => (
-                                            <tr key={file.filename} className='file-row'>
-                                                <td className="file-open-td" onClick={() => handleFileSelection(file)}>{translations?.editorNavbar.open}</td>
-                                                <td className="left-td">{file.filename}</td>
-                                                <td className="center-td">{file.created}</td>
-                                                <td className="right-td">{file.last_updated}</td>
-                                                <td className="file-hide-td" onClick={() => handleFileHiding(file)}>{translations?.editorNavbar.delete}</td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
-                            </div>
-                        )}
-                    </div>
-                </Popup>
-            </div>
-        )
-    }
 
     return (
         <div className='editornavbar' id='editornavbar'>
             <button className='editornavbar-button' onClick={handleNewFile}>{translations?.editorNavbar.newFile}</button>
             <button className="editornavbar-button" onClick={handleSaveExisting}>{translations?.editorNavbar.saveFile}</button>
             { isNewFileOpen && 
-                <NewFileScreen/>
+                <
+                    NewFileScreen
+                    isNewFileOpen={isNewFileOpen}
+                    setisNewFileOpen={setisNewFileOpen}
+                    handleSaveNew={handleSaveNew}
+                />
             }
 
             <button className="editornavbar-button" onClick={() => setisFileSelectOpen(true)}>{translations?.editorNavbar.openFile}</button>
             { isFileSelectOpen &&
-                <FileSelectionScreen/>
+                <
+                    FileSelectionScreen
+                    isFileSelectOpen={isFileSelectOpen}
+                    setisFileSelectOpen={setisFileSelectOpen}
+                    handleFileSelection={handleFileSelection}
+                    handleFileHiding={handleFileHiding}
+                    fileList={fileList}
+                />
             }
             <p tabIndex="0">{translations?.editorNavbar.file}{fileObject.filename}</p>
         </div>
---
+++ frontend/src/components/editorNavbarFileSelectionScreen.js
@@ -0,0 +1,51 @@
+import Popup from "reactjs-popup"
+import { LanguageContext } from "../contexts/languagecontext"
+import { useContext } from "react"
+
+const FileSelectionScreen = ({ isFileSelectOpen, setisFileSelectOpen, handleFileSelection, handleFileHiding, fileList, }) => {
+    const { translations } = useContext(LanguageContext)
+    return (
+        <div className="overlay" id="overlay">
+            <Popup
+                open= {isFileSelectOpen}
+                closeOnDocumentClick={false}
+                overlayStyle={{ background: 'rgba(0,0,0,0.8'}}
+            >
+                <div className='content-file-select' id="content-file-select" role = "dialog" aria-label="choose file window">
+                    <div className="content-file-select-header">
+                        <h2 tabIndex="0">{translations?.editorNavbar.chooseFile}</h2>
+                        <button className='close-button-file-select' onClick={() => setisFileSelectOpen(false)}>X</button>
+                    </div>
+                    { fileList && (
+                        <div>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th className="center-th">{translations?.editorNavbar.fileName}</th>
+                                        <th className="center-th">{translations?.editorNavbar.createdAt}</th>
+                                        <th className="center-th">{translations?.editorNavbar.lastEdited}</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {fileList.map(file => (
+                                        <tr key={file.filename} className='file-row'>
+                                            <td className="file-open-td" onClick={() => handleFileSelection(file)}>{translations?.editorNavbar.open}</td>
+                                            <td className="left-td">{file.filename}</td>
+                                            <td className="center-td">{file.created}</td>
+                                            <td className="right-td">{file.last_updated}</td>
+                                            <td className="file-hide-td" onClick={() => handleFileHiding(file)}>{translations?.editorNavbar.delete}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    )}
+                </div>
+            </Popup>
+        </div>
+    )
+}
+
+export default FileSelectionScreen
