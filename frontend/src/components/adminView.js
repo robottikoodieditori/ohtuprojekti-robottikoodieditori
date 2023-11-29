793788260218489f5eb3592e93206efc0d7bdc1b
Meeri <meeri.simpanen@helsinki.fi>
2023-11-29 11:53:32 +0200

    fixed close button

--- frontend/src/components/adminView.js
+++ frontend/src/components/adminView.js
@@ -117,7 +117,7 @@ const AdminView = () => {
                                 required
                                 id='uploadFile'
                             />
-                            <button type="submit" value="Upload" onClick={handleUpload}>Upload</button>
+                            <button type="submit" value="Upload" onClick={handleUpload}>{translations?.adminView.upload}</button>
                         </form>
                     </div>
                 </Popup>
@@ -348,12 +348,12 @@ const AdminView = () => {
                             <tbody>
                                 {allFiles.map(file => (
                                     <tr id="file-row" key={file.filename} className={file.visible ? 'visible-file' : 'hidden-file'}>
-                                        <td id="filename">{file.filename}</td>
-                                        <td id="username">{users.find(user => user.id === file.user_id).name}</td>
-                                        <td className={file.visible ? 'file-button' : 'hidden-file-button'} id="open-button" onClick={() => handleFileClick(file)}>{translations?.editorNavbar.open}</td>
-                                        <td className={file.visible ? 'file-button' : 'hidden-file-button'} id="hide-button" onClick={() => handleVisibleClick(file)}>{file.visible ? translations?.adminView.hide : translations?.adminView.restore}</td>
-                                        <td className={file.visible ? 'file-button' : 'hidden-file-button'} id="delete-button" onClick={() => handleDeleteClick(file)}>{translations?.editorNavbar.delete}</td>
-                                        <td className={file.visible ? 'file-button' : 'hidden-file-button'} id="download-button" onClick={() => handleDownloadClick(file)}>{translations?.adminView.download}</td>
+                                        <td tabIndex="0" id="filename">{file.filename}</td>
+                                        <td tabIndex="0"  id="username">{users.find(user => user.id === file.user_id).name}</td>
+                                        <td tabIndex="0" className={file.visible ? 'file-button' : 'hidden-file-button'} id="open-button" onClick={() => handleFileClick(file)}>{translations?.editorNavbar.open}</td>
+                                        <td tabIndex="0" className={file.visible ? 'file-button' : 'hidden-file-button'} id="hide-button" onClick={() => handleVisibleClick(file)}>{file.visible ? translations?.adminView.hide : translations?.adminView.restore}</td>
+                                        <td tabIndex="0" className={file.visible ? 'file-button' : 'hidden-file-button'} id="delete-button" onClick={() => handleDeleteClick(file)}>{translations?.editorNavbar.delete}</td>
+                                        <td tabIndex="0" className={file.visible ? 'file-button' : 'hidden-file-button'} id="download-button" onClick={() => handleDownloadClick(file)}>{translations?.adminView.download}</td>
                                     </tr>
                                 ))}
                             </tbody>
