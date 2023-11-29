fa3843d7f0d36c6e8e249bf2b6047c0906882a73
Joonas Maisala <joonas-maisala@hotmail.com>
2023-11-29 13:36:51 +0200

    Frontend components; Button, Editor, FileSelectionScreen, NewFileScreen commented/documented

--- frontend/src/components/editor.js
+++ frontend/src/components/editor.js
@@ -1,5 +1,11 @@
+// Editor.js
+// Implements the main code editor interface using CodeMirror. It manages the editing,
+// display, and interactions within the code editor, including handling updates to the document,
+// error underlining, autocompletion, and hover tooltips.
+
 import { useEffect, useRef, useContext } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+// CodeMirror imports for editor view, keymap, and placeholder functionalities.
 import { EditorView, keymap, placeholder } from '@codemirror/view';
 import { EditorState, Compartment } from '@codemirror/state';
 import { defaultKeymap, insertTab } from '@codemirror/commands';
@@ -19,8 +25,9 @@ const Editor = ({ textContent }) => {
     const dispatch = useDispatch()
     const serverResponse = useSelector(state => state.comms.responseFromServer)
     const { language } = useContext(LanguageContext)
-    const curWord = useRef('')
-    const editor = useRef(null)
+    // useRef hooks for managing editor state and configurations
+    const curWord = useRef('') // Stores the currently highlighted word
+    const editor = useRef(null)// Reference to the CodeMirror editor instance
     const errorListRef = useRef([])
     const languageRef = useRef('')
     const currentAutoCompleteModule = useRef(language === 'en' ? autoComplete_en : autoComplete_fi)
@@ -29,8 +36,10 @@ const Editor = ({ textContent }) => {
     const fileObject = useSelector((state) => state.editor.fileObject)
     const exampleString = 'Logo...'
 
-
+    // Update listener for the editor to handle document changes.
     const onUpdate = EditorView.updateListener.of((v) => {
+        // Logic to handle document changes, including dispatching updated content
+        // and managing custom keywords for autocompletion.
         if (v.docChanged) {
             dispatch(setContent(v.state.doc.toString()))
             const curCustomKeywords = getCustomKeywords(v.state.doc.toString())
@@ -38,27 +47,37 @@ const Editor = ({ textContent }) => {
         }
     })
 
+    // Function to dynamically update autocomplete suggestions based on the editor's content.
     const updateAutocompletes = (newList, view) => {
+        // Updates the autocomplete configuration in the editor.
         const updatedConfig = { override: [currentAutoCompleteModule.current(newList)]}
         view.dispatch({effects: autoCompletionCompartment.reconfigure(autocompletion(updatedConfig))})
     }
 
+    // Function to update hover tooltips in the editor based on the error list.
     const updateHovering = (errorList, view, language) => {
+        // Updates hover tooltip configurations for displaying error messages.
         errorListRef.current = errorList
         const updatedConfig = hoverCompartment.of(wordHover(updateLocal, errorListRef, language))
         view.dispatch({effects: hoverCompartment.reconfigure(updatedConfig)})
     }
 
+    // Update the currently hovered word in the editor.
     const updateLocal = (word) => curWord.current = word;
     const resetLocal = () => curWord.current = '';
 
+    // Handle editor click event to highlight a word and reset the local word tracker.
     const handleClick = () => {
+        // Dispatches the highlighted word to the Redux store for further processing.
         if (curWord.current !== '') {
             dispatch(setHighlightedWord(curWord.current));
             resetLocal();
         }
     }
+
+    // useEffect hook to handle error underlining and hover tooltip updates.
     useEffect(() => {
+        // Clears and updates underlines in the editor based on server response errors.
         if (serverResponse.raw_errors && editor.current && serverResponse) {
             clearUnderlines(editor.current)
             underlineSelection(editor.current, serverResponse.raw_errors)
@@ -66,39 +85,45 @@ const Editor = ({ textContent }) => {
         }
     }, [serverResponse])
 
+    // useEffect for initializing the CodeMirror editor.
     useEffect(() => {
-
+        // Creates an instance of EditorState with initial document content and extensions.
+        // - `extensions` include basic functionalities like syntax highlighting, linting, etc.
+        // - `placeholder` shows a default text when the editor is empty.
+        // - `onUpdate` handles updates to the document, such as changes in content.
+        // - `hoverCompartment` manages tooltips for words and error messages.
+        // - `autoCompletionCompartment` configures dynamic autocompletion.
+        // - `keymap` sets up custom keybindings, including handling the Tab key.
+        // - Additional configurations for multiple selections and tab size.        
         let state = EditorState.create({
             doc: textContent,
             extensions: [
-                extensions,
-                placeholder(exampleString),
-                onUpdate,
-                hoverCompartment.of(wordHover(updateLocal, errorListRef, languageRef)),
+                extensions, 
+                placeholder(exampleString), 
+                onUpdate, 
+                hoverCompartment.of(wordHover(updateLocal, errorListRef, languageRef)), 
                 autoCompletionCompartment.of(
-                    autocompletion({override: [currentAutoCompleteModule.current([])]}
-                    )),
-                keymap.of([
-                    defaultKeymap,
-                    {
-                        key: 'Tab',
-                        run: insertTab
-                    }
-                ],
+                    autocompletion({override: [currentAutoCompleteModule.current([])]}), 
                 ),
+                keymap.of([defaultKeymap, { key: 'Tab', run: insertTab }]), 
                 EditorState.allowMultipleSelections.of(false),
                 EditorState.tabSize.of(4),
             ]
-        })
-        let view = new EditorView({ state: state, parent: document.querySelector('#editor') })
-        editor.current = view
+        });
 
-        return () => {
-            view.destroy()
-        }
-    }, [])
+        // Initializes the EditorView and attaches it to the DOM.
+        // The editor instance is stored in `editor.current` for later use.        
+        let view = new EditorView({ state: state, parent: document.querySelector('#editor') });
+        editor.current = view;
 
+        // Cleanup function to destroy the editor instance when the component unmounts.
+        return () => { view.destroy(); };
+    }, []);
+
+
+    // Update editor content when the file object (filename or text content) changes.
     useEffect(() => {
+        // Dispatch changes to update the editor with the new file content.
         editor.current.dispatch({changes: {from: 0, to: editor.current.state.doc.length, insert: ''}})
         if (fileObject.filename !== '') {
             editor.current.dispatch({changes: {from: 0, to: editor.current.state.doc.length, insert: fileObject.textContent}})
@@ -106,13 +131,16 @@ const Editor = ({ textContent }) => {
 
     }, [fileObject.filename])
 
+    // Update the autocomplete module and language reference when the language changes.
     useEffect(() => {
+        // Adjusts autocomplete suggestions based on the selected language.
         currentAutoCompleteModule.current = language === 'en' ? autoComplete_en : autoComplete_fi
         languageRef.current = language
     }, [language])
 
-    //adminView.js editor useEffect
+    // Update the editor content based on the `textContent` prop, allowing external content updates.
     useEffect(() => {
+        // Dispatch changes to the editor to update its content based on `textContent`.
         if (editor.current && textContent !== undefined) {
             editor.current.dispatch({
                 changes: {
@@ -122,7 +150,7 @@ const Editor = ({ textContent }) => {
                 }
             });
         }
-    }, [textContent]); // Dependency array includes textContent
+    }, [textContent]);
 
     return (
         <div>
--- frontend/src/components/editorNavbarFileSelectionScreen.js
+++ frontend/src/components/editorNavbarFileSelectionScreen.js
@@ -1,23 +1,38 @@
-import Popup from "reactjs-popup"
-import { LanguageContext } from "../contexts/languagecontext"
-import { useContext } from "react"
+/**
+ * FileSelectionScreen.js
+ * This component renders a popup screen for file selection and deletion. It allows users to select
+ * a file to open or delete from the provided file list.
+ * 
+ * Props:
+ * - isFileSelectOpen: Boolean indicating if the popup is open.
+ * - setisFileSelectOpen: Function to set the state for opening/closing the popup.
+ * - handleFileSelection: Function to handle the event when a file is selected.
+ * - handleFileHiding: Function to handle the event when a file is requested to be deleted.
+ * - fileList: Array of file objects to be displayed.
+ * 
+ * Uses LanguageContext for internationalization, enabling the display of text in different languages.
+ */
 
-const FileSelectionScreen = ({ isFileSelectOpen, setisFileSelectOpen, handleFileSelection, handleFileHiding, fileList, }) => {
-    const { translations } = useContext(LanguageContext)
+import Popup from "reactjs-popup";
+import { LanguageContext } from "../contexts/languagecontext";
+import { useContext } from "react";
+
+const FileSelectionScreen = ({ isFileSelectOpen, setisFileSelectOpen, handleFileSelection, handleFileHiding, fileList }) => {
+    const { translations } = useContext(LanguageContext);
 
     return (
         <div className="overlay" id="overlay">
             <Popup
-                open= {isFileSelectOpen}
+                open={isFileSelectOpen}
                 closeOnDocumentClick={false}
-                overlayStyle={{ background: 'rgba(0,0,0,0.8'}}
+                overlayStyle={{ background: 'rgba(0,0,0,0.8)' }}
             >
-                <div className='content-file-select' id="content-file-select" role = "dialog" aria-label="choose file window">
+                <div className='content-file-select' id="content-file-select" role="dialog" aria-label="choose file window">
                     <div className="content-file-select-header">
                         <h2 tabIndex="0">{translations?.editorNavbar.chooseFile}</h2>
-                        <button className='close-button-file-select' onClick={() => setisFileSelectOpen(false)}>X</button>
+                        <button className='close-button-file-select' onClick={() => setisFileSelectOpen(false)}>X</button> {/* Close button to hide the popup */}
                     </div>
-                    { fileList && (
+                    {fileList && (
                         <div>
                             <table>
                                 <thead>
@@ -32,11 +47,11 @@ const FileSelectionScreen = ({ isFileSelectOpen, setisFileSelectOpen, handleFile
                                 <tbody>
                                     {fileList.map(file => (
                                         <tr key={file.filename} className='file-row'>
-                                            <td className="file-open-td" onClick={() => handleFileSelection(file)}>{translations?.editorNavbar.open}</td>
+                                            <td className="file-open-td" onClick={() => handleFileSelection(file)}>{translations?.editorNavbar.open}</td>{/* Open file button */}
                                             <td className="left-td">{file.filename}</td>
                                             <td className="center-td">{file.created}</td>
                                             <td className="right-td">{file.last_updated}</td>
-                                            <td className="file-hide-td" onClick={() => handleFileHiding(file)}>{translations?.editorNavbar.delete}</td>
+                                            <td className="file-hide-td" onClick={() => handleFileHiding(file)}>{translations?.editorNavbar.delete}</td> {/* Delete file button */}
                                         </tr>
                                     ))}
                                 </tbody>
@@ -46,7 +61,7 @@ const FileSelectionScreen = ({ isFileSelectOpen, setisFileSelectOpen, handleFile
                 </div>
             </Popup>
         </div>
-    )
-}
+    );
+};
 
-export default FileSelectionScreen
+export default FileSelectionScreen;
