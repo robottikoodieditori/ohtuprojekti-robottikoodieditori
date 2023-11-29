eff227332b2180133a06fda92d2d01f2a5070e65
Jussi Rantala <jussi.v.rantala@helsinki.fi>
2023-11-29 16:57:44 +0200

    refactoring adminview

--- frontend/src/components/adminViewUserInfo.js
+++ frontend/src/components/adminViewUserInfo.js
@@ -1,40 +1,9 @@
 import { useContext } from 'react';
 import { LanguageContext } from "../contexts/languagecontext";
-import Popup from 'reactjs-popup';
+import PasswordWindow from './passwordWindow';
 
 const AdminViewUserInfo = ({ selectedUser, isPasswordWindowOpen, setIsPasswordWindowOpen, handlePasswordChange /*, PasswordWindow*/}) => {
     const { translations } = useContext(LanguageContext)
-
-    const PasswordWindow = () => {
-        return (
-            <div className='overlay'>
-                <Popup
-                    open={isPasswordWindowOpen}
-                    closeOnDocumentClick={false}
-                    overlayStyle={{ background: 'rgba(0,0,0,0.8)' }}
-                >
-                    <div className='content-upload'>
-                        <div className="content-upload-header ">
-                            <h2 tabIndex="0">{translations?.adminView.changePassword}</h2>
-                            <div className='upload-header'>
-                                <button className="close-button-upload" onClick={() => setIsPasswordWindowOpen(false)}>X</button>
-                            </div>
-                        </div>
-                        <form onSubmit={handlePasswordChange}>
-                            <input
-                                type='text'
-                                id='passwordInput'
-                                name='password'
-                                placeholder={selectedUser.password}
-                            />
-                            <button type="submit" value="Change">Vaihda</button>
-                        </form>
-                    </div>
-                </Popup>
-            </div>
-        )
-    }
-
 
     return (
         <div className="user-info">
@@ -43,7 +12,7 @@ const AdminViewUserInfo = ({ selectedUser, isPasswordWindowOpen, setIsPasswordWi
 
             <button className="change-password-button" onClick={() => {setIsPasswordWindowOpen(true)}}> Vaihda salasana </button>
             { isPasswordWindowOpen &&
-                <PasswordWindow />
+                <PasswordWindow isPasswordWindowOpen={isPasswordWindowOpen} setIsPasswordWindowOpen={setIsPasswordWindowOpen} handlePasswordChange={handlePasswordChange} selectedUser={selectedUser} />
             }
         </div>
     )
