7afbcd664714930c5a4cc321d42bef765a9d06ca
Jussi Rantala <jussi.v.rantala@helsinki.fi>
2023-12-07 11:16:07 +0200

    cleaned up mockcompiler

--- backend/src/mockcompiler.py
+++ backend/src/mockcompiler.py
@@ -8,40 +8,10 @@ class MockCompiler:
         self.all_errors = []
 
     @staticmethod
-    def create_error_list(lines_of_logo):
-        errors = []
-        errors_with_raw_pos = []
-        pattern = r"(?:\s|^)([^\s]*[A-Z][^\s]*)(?=\s|$)"
-        split_logo = lines_of_logo.split("\n")
-
-        for line_number, line in enumerate(split_logo):
-            matches = re.finditer(pattern, line)
-
-            for match in matches:
-                errors.append(
-                    {
-                        "line": line_number + 1,
-                        "message": "error",
-                        "start": match.start(1) + 1,
-                        "end": match.end(1),
-                    }
-                )
-
-        # lines_of_logo = lines_of_logo.replace('\n', '')
-        matches = re.finditer(pattern, lines_of_logo)
-
-        for match in matches:
-            errors_with_raw_pos.append(
-                {"message": "error", "start": match.start(1), "end": match.end(1)}
-            )
-
-        return {"errors": errors, "raw_errors": errors_with_raw_pos}
-
-    @staticmethod
-    def compile2(code: str, output_file: str):
+    def compile(code: str, output_file: str):
         path = os.getcwd()
-        if path.endswith('src'):
-            path = os.path.join(path, '..')
+        if path.endswith("src"):
+            path = os.path.join(path, "..")
         path = os.path.join(path, "logomotion_gradle", "src", "main", "java", "logo")
         path += "/"
         print(path)
